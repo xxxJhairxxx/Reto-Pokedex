0320122f98c1ac25d5f56720429e096d0a5b17cd
xxxjhairxxx <jinfanzonquispe30@gmail.com>
2023-01-24 18:06:31 -0500

    Busqueda de pokedex

--- index.js
+++ index.js
@@ -3,37 +3,52 @@ const pokename= document.getElementById('pokename');
 const display = document.getElementById('display');
 const subdisplay= document.getElementById('subdisplay');
 
-const obtenerPokemones = async () => {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
+const obtenerPokemones = async (name) => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=200");
     const data = await response.json();
-    setPokemonsInView(data.results);
+    
+    localStorage.setItem('PokeList',JSON.stringify(data.results))
+    setPokemonsInView(data.results,name);
     
   };
 
   const setPokemonsInView = (results,name) => {
-    
     results.forEach(element => {
-        if(element.name=='bulbasaur'){
-            obtenerDetallePokemon('bulbasaur');
-            return ;
+        if(element.name==name){
+            obtenerDetallePokemon(name);
+        }else{
+            console.log(element.name);
         }
     });
     
   };
 
   const obtenerDetallePokemon = async (pokemon) => {
-    const p = document.createElement('p');
-
+    
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     const data = await response.json();
 
     pokeimg.setAttribute('src',`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`);
     pokename.value= (data.name).toUpperCase();
-    p.textContent= `HABILIDAD: ${data.abilities.map(element=>element.ability.name.toUpperCase()+' ')} PESO: ${data.weight}  TIPO: ${data.types.map(element=>element.type.name.toUpperCase()+' ')}`;
-    subdisplay.appendChild(p);
+    subdisplay.innerHTML= `<p>HABILIDAD: ${data.abilities.map(element=>element.ability.name.toUpperCase()+' ')} PESO: ${data.weight}  TIPO: ${data.types.map(element=>element.type.name.toUpperCase()+' ')} </p>`;
+    
 
     console.log("detalle", data);
   };
 
 
-  obtenerPokemones();
+
+
+
+  pokename.addEventListener("keypress", function(event) {
+    
+    if (event.key === "Enter") {
+      event.preventDefault();
+      if(localStorage.getItem('PokeList')){
+        const data = JSON.parse(localStorage.getItem('PokeList'));
+        setPokemonsInView(data,this.value);
+      }
+      obtenerPokemones(this.value);
+    }
+  });
+  
